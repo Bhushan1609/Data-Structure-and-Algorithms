c3f7428679138349c4e2a6690958e4218121a8d4
Bhushan Thombare <125019953+thombarebhushan@users.noreply.github.com>
2023-11-11 14:25:02 +0530

    Update 13_countSubarrayWithSumK.cpp

--- ArrayMedium/13_countSubarrayWithSumK.cpp
+++ ArrayMedium/13_countSubarrayWithSumK.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 //O(n*n*n)
@@ -22,7 +21,7 @@ void callbetter(vector < int > & v, int target) {
     int n = v.size(); 
     int cnt = 0; 
     for (int i = 0 ; i < n; i++) { 
-        int sum = 0;arr
+        int sum = 0;
         for (int j = i; j < n; j++) { 
             sum += v[j];
             if (sum == target) cnt++;
