f55e1f23e5de72b69d79ece4321a129882704859
Bhushan Thombare <125019953+thombarebhushan@users.noreply.github.com>
2024-02-21 23:12:36 +0530

    Update 01_largestElement.cpp

--- ArrayEasy/01_largestElement.cpp
+++ ArrayEasy/01_largestElement.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arr[5] = {12,2,4,13,1};
     int maxi = INT_MIN;
-    for(int i = 0 ; i < 5 ; i++) maxi = max(maxi , arr[i]);
+    for(int i = 0 ; i < 5 ; i++) maxi = max(maxi , arr[i]); 
     cout << maxi << endl;
     return 0;
 }
