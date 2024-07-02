34043222e61c0230f49e7b3b5fe65b5635bf83eb
thombarebhushan <crackthecode1609@gmail.com>
2024-07-02 17:08:59 +0530

    A2Z

--- DSA/18_Step  Strings/Lec 1 Hard Problems/9_Count palindromic subsequence in given string/J.java
+++ DSA/18_Step  Strings/Lec 1 Hard Problems/9_Count palindromic subsequence in given string/J.java
@@ -1,1 +1,26 @@
-Bhushan  THOMBARE
+#include <bits/stdc++.h>
+#define ll long long
+#define nl <<endl;
+#define pb push_back 
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+const int INF = 1e9 + 7;
+#define ninput ll n;cin>>n;
+#define abinput ll a,b;cin>>a>>b;
+#define traverse(i,n)for(ll i=0;i<n;i++)
+#define vecinput ll n;cin>>n;vector<ll>vec(n);traverse(i, n)cin>>vec[i];
+#define vecprint(vec)traverse(i, vec.size())cout<<vec[i]<<" ";cout << endl;
+using namespace std;
+void bhushan(){
+    
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll tc=1;
+    cin>>tc;
+    while(tc--) bhushan() ;
+}
