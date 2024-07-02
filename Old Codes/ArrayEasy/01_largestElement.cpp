5ca644e80fd0ffa4b58d376f22010aa220e992f1
Bhushan Thombare <125019953+thombarebhushan@users.noreply.github.com>
2024-07-02 16:43:07 +0530

    A2Z

--- Old Codes/ArrayEasy/01_largestElement.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int arr[5] = {12,2,4,13,1};
-    int maxi = INT_MIN;
-    for(int i = 0 ; i < 5 ; i++) maxi = max(maxi , arr[i]); 
-    cout << maxi << endl;
-    return 0;
-}
