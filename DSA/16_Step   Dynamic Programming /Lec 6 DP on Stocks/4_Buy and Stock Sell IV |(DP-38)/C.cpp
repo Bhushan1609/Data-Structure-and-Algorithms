d310cdfca21f786c23bf0dd2499e9baeef8a36d7
Bhushan Thombare <crackthecode1609@gmail.com>
2024-08-06 12:37:42 +0530

    A2Z

--- DSA/16_Step   Dynamic Programming /Lec 6 DP on Stocks/4_Buy and Stock Sell IV |(DP-38)/C.cpp
+++ DSA/16_Step   Dynamic Programming /Lec 6 DP on Stocks/4_Buy and Stock Sell IV |(DP-38)/C.cpp
@@ -88,8 +88,132 @@ void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
 void No(bool t = 1) { Yes(!t); }
 void yes(bool t = 1) { print(t ? "yes" : "no"); }
 void no(bool t = 1) { yes(!t); }
+int dp[1001][2][101];
+int topDown(int index,int buy,int cap,vd<int>&prices){
+    if(index==len(prices)) return 0;
+    if(cap==0)return 0;
+    if(dp[index][buy][cap] != -1)return dp[index][buy][cap];
+    int profit=0;
+    if(buy){
+        profit=max({-prices[index]+topDown(index+1,0,cap,prices) , topDown(index+1,1,cap,prices)});
+    }
+    else{
+        profit=max({+prices[index]+topDown(index+1,1,cap-1,prices) , topDown(index+1,0,cap,prices) });
+    }
+    return dp[index][buy][cap]=profit;
+}
+int bottomUp(vd<int>&prices,int K){
+    int n=len(prices);
+    vddd<int>dp(n+1, vdd<int>(2,vd<int>(K+1,0)));
+    dp[n][0][0]=dp[n][1][0]=0;
+    FOR_R(index,0,n){
+        FOR(buy,0,2){
+            FOR(k,1,K+1){
+                int profit=0;
+                if(buy){
+                    profit=max({-prices[index]+dp[index+1][0][k] ,dp[index+1][1][k] });
+                }
+                else{
+                    profit=max({+prices[index]+dp[index+1][1][k-1] ,dp[index+1][0][k] });
+                }
+                dp[index][buy][k]=profit;
+            }
+        }
+    }
+    return dp[0][1][K];
+}
+int spaceOptimize(vd<int>&prices,int K){
+    int n=len(prices);
+    vdd<int>prev(2,vd<int>(K+1,0)),curr(2,vd<int>(K+1,0));
+    prev[0][0]=prev[1][0]=0;
+    FOR_R(index,0,n){
+        FOR(buy,0,2){
+            FOR(k,1,K+1){
+                int profit=0;
+                if(buy){
+                    profit=max({-prices[index]+prev[0][k] ,prev[1][k] });
+                }
+                else{
+                    profit=max({+prices[index]+prev[1][k-1] ,prev[0][k] });
+                }
+                curr[buy][k]=profit;
+            }
+        }
+        swap(prev,curr);
+    }
+    return prev[1][K];
+}
+
+int dp1[1001][202];
+int topDown1(int index,int check,int k,vd<int>&prices){
+    if(index==len(prices) or check==(2*k)) return 0;
+    if(dp1[index][check] != -1)return dp1[index][check];
+    int profit=0;
+    if(check % 2 == 0){
+        profit=max({-prices[index]+topDown1(index+1,check+1,k,prices) , topDown1(index+1,check,k,prices)});
+    }
+    else{
+        profit=max({+prices[index]+topDown1(index+1,check+1,k,prices) , topDown1(index+1,check,k,prices) });
+    }
+    return dp1[index][check]=profit;
+}
+int bottomUp1(vd<int>&prices,int k){
+    int n=len(prices);
+    vdd<int>dp(n+1,vd<int>(2*k+1,0));
+    FOR(j,2*k)dp[n][j]=0;
+    FOR(i,n+1)dp[i][2*k]=0;
+    FOR_R(index,0,n){
+        FOR_R(check,0,2*k){
+            int profit=0;
+            if(check % 2 == 0){
+                profit=max(-prices[index]+dp[index+1][check+1] , dp[index+1][check] );
+            }
+            else{
+                profit=max(+prices[index]+dp[index+1][check+1] , dp[index+1][check] );
+            }
+            dp[index][check]=profit;
+        }
+    }
+    return dp[0][0];
+}
+int spaceOptimize1(vd<int>&prices,int k){
+    int n=len(prices);
+    vd<int>prev(2*k+1,0),curr(2*k+1,0);
+    FOR(j,2*k)prev[j]=0;
+    FOR(i,n+1)prev[2*k]=0;
+    FOR_R(index,0,n){
+        FOR_R(check,0,2*k){
+            int profit=0;
+            if(check % 2 == 0){
+                profit=max(-prices[index]+prev[check+1] , prev[check] );
+            }
+            else{
+                profit=max(+prices[index]+prev[check+1] , prev[check] );
+            }
+            curr[check]=profit;
+        }
+        swap(prev,curr);
+    }
+    return prev[0];
+}
 void itachi_1609(){
-    
+    vd<int>prices={3,2,6,5,0,3};
+    int k=2;
+    FOR(i,1001){
+        FOR(j,2){
+            FOR(k,101){
+                dp[i][j][k]=-1;
+            }
+        }
+    }
+    print(topDown(0,1,k,prices));printn();
+    print(bottomUp(prices,k));printn();
+    print(spaceOptimize(prices,k));printn();
+
+    FOR(i,1001 * 202)dp1[i/202][i%202]=-1;
+    print(topDown1(0,0,k,prices));printn();
+    print(bottomUp1(prices,k));printn();
+    print(spaceOptimize1(prices,k));printn();
     return;
 }
 int main(){
