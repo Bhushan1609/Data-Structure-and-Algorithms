be1351fb1b9633f691483938ddfccf31e13dceb0
Bhushan Thombare <crackthecode1609@gmail.com>
2024-12-05 16:09:39 +0530

    SelectioSort Added

---
+++ DSA/02_Step  Learn Important Sorting Techniques/1_Sorting /1_Selection sort/Java.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Java{
+    static void selectionSort(Vector<Integer>arr){
+        int n=arr.size();
+        for(int i=0;i<n-2;i++){
+            int mini=i;
+            for(int j=i;j<n;j++){
+                if(arr.get(j) < arr.get(mini)){
+                    mini=j;
+                }
+            }
+            int temp=arr.get(i);
+            arr.set(i , arr.get(mini));
+            arr.set(mini,temp);
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr.get(i)+" ");
+        }
+        System.out.println();
+        return;
+    }
+    public static void main(String[] args){
+        Vector<Integer>arr=new Vector<>();
+        arr.add(13);
+        arr.add(46);
+        arr.add(24);
+        arr.add(52);
+        arr.add(20);
+        arr.add(9);
+        selectionSort(arr);
+    }
+}
