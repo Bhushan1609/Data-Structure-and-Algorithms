041f3b5fb75cb2e5d170920d4b1a67d23d0b233e
Bhushan Thombare <crackthecode1609@gmail.com>
2025-01-11 19:09:40 +0530

    Majority Element (>n/2 times) Added

--- DSA/03_Step   Solve Problems on Arrays [Easy -> Medium -> Hard]/Medium/03_Majority Element (>n/2 times)/CPP.cpp
+++ DSA/03_Step   Solve Problems on Arrays [Easy -> Medium -> Hard]/Medium/03_Majority Element (>n/2 times)/CPP.cpp
@@ -160,7 +160,8 @@ void callOptimal(vd<int>&arr){
 void solve(){
     vd<int>arr={2,2,3,3,1,2,2};
     callBrute(arr);
-    callBetter(arr);
+    callBetter(arr);   
+    
     callOptimal(arr);
 }
 
