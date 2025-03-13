66e27be58d0168d9b2623fc346b01c0ac2a540bc
Bhushan Thombare <crackthecode1609@gmail.com>
2025-03-14 01:28:14 +0530

    Implement Stack using Queue

--- DSA/09_Step   Stack and Queues /Lec 1 Learning/03_Implement Stack using Queue/CPP.cpp
+++ DSA/09_Step   Stack and Queues /Lec 1 Learning/03_Implement Stack using Queue/CPP.cpp
@@ -1,27 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class MyStack {
+class MyStack{
     queue<int>q;
 public:
-    void push(int x) {
-        int size=q.size();
+    void push(int x){
+        int rev_size=q.size();
         q.push(x);
-        for(int i=1;i<=size;i++){
+        for(int i=0;i<rev_size;i++){
             q.push(q.front());
             q.pop();
         }
+        return ;
     }
-    int pop() {
-        int val=q.front();
+    void pop(){
         q.pop();
-        return val;
     }
-    int top() {
+    int top(){
         return q.front();
     }
-    bool empty() {
-        return q.empty();
+    int size(){
+        return q.size();
     }
 };
 
@@ -30,5 +29,13 @@ int main(){
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
     #endif
+    MyStack st=MyStack();
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    cout<<st.size()<<endl;
+    st.pop();
+    cout<<st.size()<<endl;
+    cout<<st.top()<<endl;
     return 0;
-}
+}   
