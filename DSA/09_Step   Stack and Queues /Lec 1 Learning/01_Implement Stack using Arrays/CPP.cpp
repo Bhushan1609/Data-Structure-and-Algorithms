bf9db46d9d7233ad4efcb413a5e9ef3efbd3e3ca
Bhushan Thombare <crackthecode1609@gmail.com>
2025-03-13 23:58:23 +0530

    Implement Stack using Arrays

--- DSA/09_Step   Stack and Queues /Lec 1 Learning/01_Implement Stack using Arrays/CPP.cpp
+++ DSA/09_Step   Stack and Queues /Lec 1 Learning/01_Implement Stack using Arrays/CPP.cpp
@@ -3,52 +3,46 @@ using namespace std;
 
 class MyStack{
 private:
-    int arr[1000];
+    int arr[100];
     int top;
 public:
     MyStack(){
         top=-1;
     }
-    int pop(){
-        if(top!=-1){
-            return arr[top--];
-        }
-        return top;
-    }
     void push(int x){
-        if(top<1000){
-           top+=1;
-           arr[top]=x;
+        if(top+1>=100){
+            cout<<"Stack Size Exceeded"<<endl;
+            return;
         }
+        ++top;
+        arr[top]=x;
+        return;
+    }
+    void pop(){
+        if(top>-1)
+            --top;
+        return;
+    }
+    int Top(){
+        if(top==-1)
+            return -1;
+        return arr[top];
     }
     int size(){
         return top+1;
     }
-    int Top(){
-        if(top!=-1){
-            return arr[top];
-        }
-        return -1;
-    }
 };
 
 int main(){
-    #ifndef ONLINE_JUDGE
+    #ifndef ONLING_JUDGE
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
     #endif
-    MyStack myStack=MyStack();
-    cout<<myStack.Top()<<endl;
-    myStack.push(12);
-    cout<<myStack.size()<<endl;
-    myStack.push(13);
-    myStack.push(14);
-    myStack.push(15);
-    cout<<myStack.Top()<<endl;
-    myStack.pop();
-    myStack.pop();
-    cout<<myStack.Top()<<endl;
-    myStack.pop();
-    cout<<myStack.Top()<<endl;
+    MyStack st=MyStack();
+    st.push(10);
+    cout<<st.size()<<endl;
+    st.pop();
+    cout<<st.size()<<endl;
+    cout<<st.Top()<<endl;
     return 0;
-}
+}   
