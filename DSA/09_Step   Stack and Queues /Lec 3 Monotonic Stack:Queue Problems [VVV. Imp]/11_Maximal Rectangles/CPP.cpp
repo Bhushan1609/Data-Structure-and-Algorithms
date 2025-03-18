2459edf63b298dbb5c02ddf366ccff321a5db40e
Bhushan Thombare <crackthecode1609@gmail.com>
2025-03-18 14:54:36 +0530

    Maximal Rectangles

---
+++ DSA/09_Step   Stack and Queues /Lec 3 Monotonic Stack:Queue Problems [VVV. Imp]/11_Maximal Rectangles/CPP.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> prevSmallerElement(vector<int>&arr){
+    int n=arr.size();
+    stack<int>st;
+    vector<int>pse(n);
+    for(int i=0;i<n;i++){
+        if(st.empty())
+            pse[i]=-1;
+        else{
+            while(!st.empty() && arr[st.top()]>=arr[i])
+                st.pop();
+            if(st.empty())
+                pse[i]=-1;
+            else
+                pse[i]=st.top();
+        }
+        st.push(i);
+    }
+    return pse;
+}
+
+vector<int> nextSmallerElement(vector<int>&arr){
+    int n=arr.size();
+    stack<int>st;
+    vector<int>nse(n);
+    for(int i=n-1;i>=0;i--){
+        if(st.empty())
+            nse[i]=n;
+        else{
+            while(!st.empty() && arr[st.top()]>arr[i])
+                st.pop();
+            if(st.empty())
+                nse[i]=n;
+            else
+                nse[i]=st.top();
+        }
+        st.push(i);
+    }
+    return nse;
+}
+
+int callBrute(vector<int>&heights){
+    int n=heights.size();
+    int ans=0;
+    vector<int>pse=prevSmallerElement(heights),nse=nextSmallerElement(heights);
+    for(int i=0;i<n;i++)
+        ans=max(ans,(nse[i]-pse[i]-1)*heights[i]);
+    return ans;
+}
+
+int callOptimal(vector<int>&heights){
+    int n=heights.size();
+    int ans=0;
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        while(!st.empty() && heights[st.top()]>heights[i]){
+            int element=heights[st.top()];
+            st.pop();
+            int nse=i;
+            int pse=(st.empty()?-1:st.top());
+            ans=max(ans,(nse-pse-1)*element);
+        }
+        st.push(i);
+    }
+    while(!st.empty()){
+        int element=heights[st.top()];
+        st.pop();
+        int nse=n;
+        int pse=(st.empty()?-1:st.top());
+        ans=max(ans,(nse-pse-1)*element);
+    }
+    return ans;
+}
+
+int maximalRectangle(vector<vector<char>>& matrix){
+    int n=matrix.size();
+    int m=matrix[0].size();
+    vector<vector<int>>vec(n,vector<int>(m,-1));
+    int ans=0;
+    for(int j=0;j<m;j++){
+        int pref=matrix[0][j]-'0';
+        vec[0][j]=pref;
+        for(int i=1;i<n;i++){
+            if(matrix[i][j]-'0'>0){
+                pref+=(matrix[i][j]-'0');
+                vec[i][j]=pref;
+            }else{
+                pref=0;
+                vec[i][j]=0;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            ans=max(ans,callOptimal(vec[i]));
+    return ans;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    vector<vector<char>>matrix={
+        {'1','0','1','0','0'},
+        {'1','0','1','1','1'},
+        {'1','1','1','1','1'},
+        {'1','0','0','1','0'}
+    };
+    cout<<maximalRectangle(matrix)<<endl;
+    return 0;
+}
