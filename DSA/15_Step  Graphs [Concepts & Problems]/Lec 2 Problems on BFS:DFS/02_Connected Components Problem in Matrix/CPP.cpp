36c4af358159bafa8d95462bb749b706174287fe
Bhushan Thombare <crackthecode1609@gmail.com>
2025-03-31 12:32:45 +0530

    Connected Components Problem in Matrix

--- DSA/15_Step  Graphs [Concepts & Problems]/Lec 2 Problems on BFS:DFS/02_Connected Components Problem in Matrix/CPP.cpp
+++ DSA/15_Step  Graphs [Concepts & Problems]/Lec 2 Problems on BFS:DFS/02_Connected Components Problem in Matrix/CPP.cpp
@@ -3,27 +3,15 @@ using namespace std;
 
 const vector<pair<int,int>>gridMoves={{-1,0},{0,-1},{0,1},{1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
 
-void dfsRecursion(int i,int j,vector<vector<int>>&visited,vector<vector<int>>&mat){
+void dfs(int i,int j,vector<vector<int>>&visited,vector<vector<int>>&matrix){
     ++visited[i][j];
     for(int k=0;k<8;k++){
         int newi=i+gridMoves[k].first;
         int newj=j+gridMoves[k].second;
-        if(newi>=0 && newi<mat.size() && newj>=0 && newj<mat[0].size() && !visited[newi][newj] && mat[newi][newj])
-            dfsRecursion(newi,newj,visited,mat);
+        if(newi>=0 && newi<matrix.size() && newj>=0 && newj<matrix[0].size() && !visited[newi][newj] && matrix[newi][newj])
+            dfs(newi,newj,visited,matrix);
     }
     return ;
-}
-
-int DFS(vector<vector<int>>&mat){
-    int n=mat.size();
-    int m=mat[0].size();
-    vector<vector<int>>visited(n,vector<int>(m, 0));
-    int cnt=0;
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            if(!visited[i][j] && mat[i][j])
-                dfsRecursion(i,j,visited,mat),++cnt;
-    return cnt;
 }
 
 int main(){
@@ -31,7 +19,14 @@ int main(){
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
     #endif
-    vector<vector<int>>mat={{0,1},{1,0},{1,1},{1,0}};
-    cout<<"Number of Islands are : "<<DFS(mat)<<endl;
+
+    vector<vector<int>>matrix={{0,1},{1,0},{1,1},{1,0}};
+    int n=matrix.size(),m=matrix[0].size(),cnt=0;
+    vector<vector<int>>visited(n,vector<int>(m));
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            if(!visited[i][j] && matrix[i][j])
+                dfs(i,j,visited,matrix),++cnt;
+    cout<<"Numbers of Islands are : "<<cnt<<endl;
     return 0;
 }
