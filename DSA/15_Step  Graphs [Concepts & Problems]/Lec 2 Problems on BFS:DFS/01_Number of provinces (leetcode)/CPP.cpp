640b7ffe6d07affb3a72ba4a0e367d5ad221a8fd
Bhushan Thombare <crackthecode1609@gmail.com>
2025-03-31 12:16:41 +0530

    Number of provinces (leetcode)

--- DSA/15_Step  Graphs [Concepts & Problems]/Lec 2 Problems on BFS:DFS/01_Number of provinces (leetcode)/CPP.cpp
+++ DSA/15_Step  Graphs [Concepts & Problems]/Lec 2 Problems on BFS:DFS/01_Number of provinces (leetcode)/CPP.cpp
@@ -4,44 +4,42 @@ using namespace std;
 template<class T>
 class Graph{
 public:
-    T nodes;
-    T edges;
+    T nodes,edges;
     vector<pair<T,T>>*adjList;
     Graph(T n,T e){
-        this->nodes=n;
+        this->nodes=n+1;
         this->edges=e;
-        adjList=new vector<pair<T,T>>[nodes+1];
+        adjList=new vector<pair<T,T>>[this->nodes];
     }
-    void addEdge(T u,T v,T wt=0,T dir=0){
-        adjList[u].push_back({v,wt});
-        if(!dir)
-            adjList[v].push_back({u,wt});
+    void addEdge(T u,T v,T weight=0,T directed=0){
+        adjList[u].push_back({v,weight});
+        if(!directed)
+            adjList[v].push_back({u,weight});
     }
     void printGraph(){
-        for(T i=0;i<this->nodes;i++){
-            cout<<i<<":";
-            for(auto &j:adjList[i])
-                cout<<"{"<<j.first<<","<<j.second<<"},";
+        for(T node=0;node<this->nodes;node++){
+            cout<<"{"<<node<<"} : ";
+            for(auto &[child,weight]:adjList[node])
+                cout<<"{"<<child<<","<<weight<<"},";
             cout<<endl;
         }
     }
 };
 
-void dfsRecursion(int node,Graph<int>&g,vector<int>&visited){
+void dfsRecursion(int &node,vector<int>&visited,Graph<int>&g){
     ++visited[node];
-    for(auto i:g.adjList[node])
-        if(!visited[i.first])
-            dfsRecursion(i.first,g,visited);
+    for(auto &[child,weight]:g.adjList[node])
+        if(!visited[child])
+            dfsRecursion(child,visited,g);
+    return ;
 }
 
 int DFS(Graph<int>&g){
-    int nodes=g.nodes,cnt=0;
-    vector<int>visited(nodes);
-    for(int i=0;i<nodes;i++)
-        if(!visited[i]){
-            dfsRecursion(i,g,visited);
-            ++cnt;
-        }
+    int cnt=0;
+    vector<int>visited(g.nodes);
+    for(int node=0;node<g.nodes-1;node++)//g.nodes-1=>bcoz,zero based indexing
+        if(!visited[node])
+            dfsRecursion(node,visited,g),++cnt;
     return cnt;
 }
 
@@ -50,19 +48,15 @@ int main(){
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
     #endif
-    vector<vector<int>>adj={
-        {1,1,0},{1,1,0},{0,0,1}
-    };
-    int nodes=adj.size();
-    int edges=0;
-    for(int i=0;i<adj.size();i++)
-        for(int j=0;j<adj[i].size();j++)
-            edges+=(i!=j && adj[i][j]);
-    Graph g(nodes,edges);
-    for(int i=0;i<adj.size();i++)
-        for(int j=0;j<adj[i].size();j++)
-            if(i!=j && adj[i][j])
+
+    vector<vector<int>>graphInput={{1,1,0},{1,1,0},{0,0,1}};
+    int nodes=graphInput.size();
+    Graph<int>g(nodes,0);
+    for(int i=0;i<graphInput.size();i++)
+        for(int j=0;j<graphInput[i].size();j++)
+            if(i!=j && graphInput[i][j])
                 g.addEdge(i,j,0,1);
-    cout<<"Number Of Provinces are : "<<DFS(g)<<endl;
+    
+    cout<<"Number of Provinces are : "<<DFS(g)<<endl;
     return 0;
 }
