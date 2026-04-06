74f9668fa5aef16f9b439a81d24eef468a6b452d
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-04-07 00:47:26 +0530

    Selection Sort Implementationb (CPP,Java,JavaScript)- 07/04/26

--- DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/CPP.cpp
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/CPP.cpp
@@ -6,7 +6,7 @@ void selectionSort(vector<int>&arr){
     for(int i=0;i<n;i++){
         int minIndex=i;
         for(int j=i+1;j<n;j++){
-            if(arr[minIndex]>arr[j])
+            if(arr[j]<arr[minIndex])
                 minIndex=j;
         }
         swap(arr[i],arr[minIndex]);
@@ -19,19 +19,18 @@ int main(){
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
     #endif
-
+    
     vector<int>arr={13,46,24,52,20,9};
-    cout<<"Before Sorting : "<<endl;
+    cout<<"Before Sorting : ";
     for(auto &i:arr)
         cout<<i<<" ";
     cout<<endl;
 
     selectionSort(arr);
 
-    cout<<"After Sorting : "<<endl;
+    cout<<"After Sorting  : ";
     for(auto &i:arr)
         cout<<i<<" ";
     cout<<endl;
     return 0;
-} 
-//Problem Link : https://www.geeksforgeeks.org/problems/selection-sort/1
+}
--- DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/JavaScript.js
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/JavaScript.js
@@ -8,41 +8,41 @@ function readline() {
 }
 
 function swap(i,j,arr){
-	let temp=arr[i];
-	arr[i]=arr[j];
-	arr[j]=temp;
-	return ;
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    return ;
 }
 
 function selectionSort(arr){
-	let n=arr.length;
-	for(let i=0;i<n;i++){
-		let minIndex=i;
-		for(let j=i+1;j<n;j++){
-			if(arr[minIndex]>arr[j]){
-				minIndex=j;
-			}
-		}
-		swap(i,minIndex,arr);
-	}
+    let n=arr.length;
+    for(let i=0;i<n;i++){
+        let minIndex=i;
+        for(let j=i+1;j<n;j++){
+            if(arr[minIndex]>arr[j]){
+                minIndex=j;
+            }
+        }
+        swap(i,minIndex,arr);
+    }
 }
 
 function main(){
-	let arr=[13,46,24,52,20,9];
-	output.push("Before Sorting : ");
-	let oneline="";
-	for(let i in arr)
-		oneline+=arr[i]+" ";
-	output.push(oneline);
+    let arr=[13,46,24,52,20,9];
+    output.push("Before Sorting : ");
+    let oneline="";
+    for(let i in arr)
+        oneline+=arr[i]+" ";
+    output.push(oneline);
 
-	selectionSort(arr);
+    selectionSort(arr);
 
-	output.push("After Sorting : ");
-	oneline="";
-	for(let i in arr)
-		oneline+=arr[i]+" ";
-	output.push(oneline);
-	fs.writeFileSync('output.txt',output.join('\n'));
+    output.push("After Sorting : ");
+    oneline="";
+    for(let i in arr)
+        oneline+=arr[i]+" ";
+    output.push(oneline);
+    fs.writeFileSync('output.txt',output.join('\n'));
 }
 
 main();
