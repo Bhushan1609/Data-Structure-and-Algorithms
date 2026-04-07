0f0dcaddebfd132ca90f8b83f11ceacadde8aaf8
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-04-07 15:51:38 +0530

    Selection Sort Implementationb (CPP,Java,JavaScript)- 08/04/26

--- DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/Java.java
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/Java.java
@@ -1,13 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-public class Java{
-	
-	static void swap(int i,int j,List<Integer>arr){
+class Java{
+
+	static void swap(List<Integer>arr,int i,int j){
 		int temp=arr.get(i);
 		arr.set(i,arr.get(j));
 		arr.set(j,temp);
-		return ;
 	}
 
 	static void selectionSort(List<Integer>arr){
@@ -15,32 +14,31 @@ public class Java{
 		for(int i=0;i<n;i++){
 			int minIndex=i;
 			for(int j=i+1;j<n;j++){
-				if(arr.get(minIndex)>arr.get(j)){
+				if(arr.get(j)<arr.get(minIndex))
 					minIndex=j;
-				}
 			}
-			swap(i,minIndex,arr);
+			Collections.swap(arr, i, minIndex);
 		}
 	}
-
 	public static void main(String args[]) throws FileNotFoundException{
-		try(
-			Scanner in = new Scanner(new File("input.txt"));
-			PrintWriter out = new PrintWriter("output.txt")
-		){
-			List<Integer>arr=new ArrayList<>(Arrays.asList(13,46,24,52,20,9));
-			out.println("Before Sorting : ");
-			for(int i:arr)
-				out.print(i+" ");
-			out.println();
+		{
+			try(
+				Scanner in=new Scanner(new File("input.txt"));
+				PrintWriter out=new PrintWriter("output.txt")
+			){
+				List<Integer>arr=new ArrayList<>(Arrays.asList(13,46,24,52,20,9));
+				out.print("Before Sorting : ");
+				for(int i:arr)
+					out.print(i+" ");
+				out.println(); 
 
-			selectionSort(arr);
+				selectionSort(arr);  
 
-			out.println("After Sorting : ");
-			for(int i:arr)
-				out.print(i+" ");
-			out.println();
+				out.print("After Sorting  : ");
+				for(int i:arr)
+					out.print(i+" ");
+				out.println();         
+			}
 		}
 	}
-} 
-//Problem Link : https://www.geeksforgeeks.org/problems/selection-sort/1
+}
