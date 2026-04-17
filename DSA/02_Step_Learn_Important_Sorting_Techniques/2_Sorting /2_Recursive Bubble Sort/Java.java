dd5101233037a089ae442298162be19aacc96159
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-04-17 07:25:32 +0530

    Bubble Sort Implementationb (CPP,Java,JavaScript)- 17/04/26

--- DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /2_Recursive Bubble Sort/Java.java
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /2_Recursive Bubble Sort/Java.java
@@ -1,29 +1,40 @@
+import java.io.*;
 import java.util.*;
-public class Java{
-    static void recursion(int i,Vector<Integer>arr){
-        if(i<0)return;
-        for(int j=0;j<i;j++){
-            if(arr.get(j)>arr.get(j+1)){
-                int temp=arr.get(j+1); 
-                arr.set(j+1,arr.get(j));
-                arr.set(j,temp);
-            }
-        }
-        recursion(i-1,arr);
-    }
-    static void recursiveBubbleSort(Vector<Integer>arr){
-        recursion(arr.size()-1,arr);
-        return;
-    }
-    public static void main(String[] args){
-        Vector<Integer>arr=new Vector<>();
-        arr.add(13);
-        arr.add(46);
-        arr.add(24);
-        arr.add(52);
-        arr.add(20);
-        arr.add(9);
-        recursiveBubbleSort(arr);
-        System.out.println(arr);
-    }
+ 
+class Java{
+
+	static void bubbleSort(List<Integer>arr){
+		int n=arr.size();
+		for(int i=n-1;i>0;i--){
+			boolean swapHappens=false;
+			for(int j=0;j<i;j++){
+				if(arr.get(j)>arr.get(j+1)){
+					swapHappens|=true;
+					Collections.swap(arr,j,j+1);
+				}
+			}
+		}
+	}
+	public static void main(String args[]) throws FileNotFoundException{
+		{
+			try(
+				Scanner in=new Scanner(new File("input.txt"));
+				PrintWriter out=new PrintWriter("output.txt")
+			){
+				List<Integer>arr=new ArrayList<>(Arrays.asList(13,46,24,52,20,9));
+				out.print("Before Sorting : ");
+				for(int i:arr)
+					out.print(i+" ");
+				out.println(); 
+
+				bubbleSort(arr);  
+
+				out.print("After Sorting  : ");
+				for(int i:arr)
+					out.print(i+" ");
+				out.println();         
+			}
+		}
+	}
 }
+//Problem Link : http://geeksforgeeks.org/problems/bubble-sort/1
--- DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /2_Recursive Bubble Sort/JavaScript.js
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /2_Recursive Bubble Sort/JavaScript.js
@@ -1,18 +1,46 @@
-let arr=[13,46,24,52,20,9];
-recursiveBubbleSort(arr);
-console.log(arr);
+const fs=require('fs');
+let inputString=fs.readFileSync('input.txt','utf-8').split('\n').filter(line=>line.trim()!=='');
+let currentLine=0;
+let output=[];
 
-function recursion(arr,i){
-    if(i<0)return;
-    for(let j=0;j<i;j++){
-        if(arr[j]>arr[j+1]){
-            let temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
+function readline() {
+    return inputString[currentLine++] ;
+}
+
+function bubbleSort(arr){
+    let n=arr.length;
+    for(let i=n-1;i>0;i--){
+        let swapHappens=false;
+        for(let j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                swapHappens|=true;
+                let temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
         }
+        if(!swapHappens)
+            break;
     }
-    recursion(arr,i-1);
 }
-function recursiveBubbleSort(arr){            
-    recursion(arr,arr.length-1);
+
+function main(){
+    let arr=[13,46,24,52,20,9];
+    output.push("Before Sorting : ");
+    let oneline="";
+    for(let i in arr)
+        oneline+=arr[i]+" ";
+    output.push(oneline);
+
+    bubbleSort(arr);
+
+    output.push("After Sorting : ");
+    oneline="";
+    for(let i in arr)
+        oneline+=arr[i]+" ";
+    output.push(oneline);
+    fs.writeFileSync('output.txt',output.join('\n'));
 }
+
+main();
+//Problem Link : https://www.geeksforgeeks.org/problems/selection-sort/1
