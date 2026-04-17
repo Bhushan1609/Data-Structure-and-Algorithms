785365339a3bad0597c2953a8c60e6238ff1a9c8
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-04-17 07:26:46 +0530

    Insertion Sort Implementationb (CPP,Java,JavaScript)- 17/04/26

--- DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /3_Recursive Insertion Sort/Java.java
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /3_Recursive Insertion Sort/Java.java
@@ -1,29 +1,38 @@
+import java.io.*;
 import java.util.*;
-public class Java{
-    static void recursion(int i,Vector<Integer>arr){
-        if(i>=arr.size())return;
-        int j=i;
-        while(j>0 && arr.get(j-1) > arr.get(j)){
-            int temp=arr.get(j-1);
-            arr.set(j-1,arr.get(j));
-            arr.set(j,temp);
-            j-=1;
+
+class Java{
+
+	static void insertionSort(List<Integer>arr){
+		int n=arr.size();
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && arr.get(j)<arr.get(j-1)){
+                Collections.swap(arr,j,j-1);
+                j--;
+            }
         }
-        recursion(i+1,arr);
-    }
-    static void recursiveInsertionSort(Vector<Integer>arr){
-        recursion(0,arr);
-        return;
-    }
-    public static void main(String[] args){
-        Vector<Integer>arr=new Vector<>();
-        arr.add(13);
-        arr.add(46);
-        arr.add(24);
-        arr.add(52);
-        arr.add(20);
-        arr.add(9);
-        recursiveInsertionSort(arr);
-        System.out.println(arr);
-    }
+	}
+	public static void main(String args[]) throws FileNotFoundException{
+		{
+			try(
+				Scanner in=new Scanner(new File("input.txt"));
+				PrintWriter out=new PrintWriter("output.txt")
+			){
+				List<Integer>arr=new ArrayList<>(Arrays.asList(13,46,24,52,20,9));
+				out.print("Before Sorting : ");
+				for(int i:arr)
+					out.print(i+" ");
+				out.println(); 
+
+				insertionSort(arr);  
+
+				out.print("After Sorting  : ");
+				for(int i:arr)
+					out.print(i+" ");
+				out.println();         
+			}
+		}
+	}
 }
+//Problem Link : https://www.geeksforgeeks.org/problems/insertion-sort/1
--- DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /3_Recursive Insertion Sort/JavaScript.js
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/2_Sorting /3_Recursive Insertion Sort/JavaScript.js
@@ -1,18 +1,43 @@
-let arr=[13,46,24,52,20,9];
-recursiveInsertionSort(arr);
-console.log(arr);
+const fs=require('fs');
+let inputString=fs.readFileSync('input.txt','utf-8').split('\n').filter(line=>line.trim()!=='');
+let currentLine=0;
+let output=[];
 
-function recursion(arr,i){
-    if(i>=arr.length)return;
-    let j=i;
-    while(j>0 && arr[j-1]>arr[j]){
-        let temp=arr[j-1];
-        arr[j-1]=arr[j];
-        arr[j]=temp;
-        j-=1;
+function readline() {
+    return inputString[currentLine++] ;
+}
+
+function insertionSort(arr){
+    let n=arr.length;
+    for(let i=0;i<n;i++){
+        let j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            let temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
     }
-    recursion(arr,i+1);
+    return;
 }
-function recursiveInsertionSort(arr){
-    recursion(arr,0);
+
+function main(){
+    let arr=[13,46,24,52,20,9];
+    output.push("Before Sorting : ");
+    let oneline="";
+    for(let i in arr)
+        oneline+=arr[i]+" ";
+    output.push(oneline);
+
+    insertionSort(arr);
+
+    output.push("After Sorting : ");
+    oneline="";
+    for(let i in arr)
+        oneline+=arr[i]+" ";
+    output.push(oneline);
+    fs.writeFileSync('output.txt',output.join('\n'));
 }
+
+main();
+//Problem Link : https://www.geeksforgeeks.org/problems/insertion-sort/1
