1b3f7e59aa54e123516b8323b28671c985537513
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-09-02 11:55:31 +0530

    Selection Sort

--- DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/CPP.cpp
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/01_Sorting /01_Selection sort/CPP.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void selectionSort(vector<int>&arr){
-    int n=arr.size();
-    for(int i=0;i<n;i++){
+void selection_sort(vector<int>&vec){
+    int n=vec.size();
+    for(int i=0;i<n-1;i++){
         int minIndex=i;
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[minIndex])
+        for(int j=i;j<n;j++){
+            if(vec[j]<vec[minIndex])
                 minIndex=j;
         }
-        swap(arr[i],arr[minIndex]);
+        swap(vec[i],vec[minIndex]);
     }
-    return ;
+    return;
 }
 
 int main(){
@@ -19,19 +19,21 @@ int main(){
         freopen("input.txt","r",stdin);
         freopen("output.txt","w",stdout);
     #endif
-    
-    vector<int>arr={13,46,24,52,20,9};
+
+    vector<int>vec={13,46,24,52,20,9};
+
     cout<<"Before Sorting : ";
-    for(auto &i:arr)
-        cout<<i<<" ";
+    for(auto &ele:vec)
+        cout<<ele<<" ";
     cout<<endl;
 
-    selectionSort(arr);
+    selection_sort(vec);
 
-    cout<<"After Sorting  : ";
-    for(auto &i:arr)
-        cout<<i<<" ";
+    cout<<"After Sorting : ";
+    for(auto &ele:vec)
+        cout<<ele<<" ";
     cout<<endl;
     return 0;
 }
+
 //Problem Link : https://www.geeksforgeeks.org/problems/selection-sort/1
