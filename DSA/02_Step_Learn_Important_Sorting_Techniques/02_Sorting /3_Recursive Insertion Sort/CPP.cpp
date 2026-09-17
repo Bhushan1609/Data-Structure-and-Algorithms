7ace3b07dd8d86abd07aa88e43bb4bf0a86496a3
Bhushan Thombare <bhushan.thombare.cs1609@gmail.com>
2026-09-17 17:15:33 +0530

    Recursive Insertion Sort added.

--- DSA/02_Step_Learn_Important_Sorting_Techniques/02_Sorting /3_Recursive Insertion Sort/CPP.cpp
+++ DSA/02_Step_Learn_Important_Sorting_Techniques/02_Sorting /3_Recursive Insertion Sort/CPP.cpp
@@ -1,16 +1,19 @@
+//Problem Link : https://www.geeksforgeeks.org/problems/insertion-sort/1
+
 #include<bits/stdc++.h>
 using namespace std;
 
-void insertionSort(vector<int>&arr){
-    int n=arr.size();
-    for(int i=0;i<n;i++){
-        int j=i;
-        while(j>0 && arr[j]<arr[j-1]){
-            swap(arr[j],arr[j-1]);
-            j--;
-        }
+void recusive_insertion_sort(vector<int>&vec,int i){
+    if(i>=vec.size())
+        return ;
+    
+    int j=i;
+    while(j>0 && vec[j]<vec[j-1]){
+        swap(vec[j],vec[j-1]);
+        j--;
     }
-    return;
+    recusive_insertion_sort(vec,i+1);
+    return ;
 }
 
 int main(){
@@ -19,18 +22,19 @@ int main(){
         freopen("output.txt","w",stdout);
     #endif
     
-    vector<int>arr={13,46,24,52,20,9};
+    vector<int>vec={13,46,24,52,20,9};
+
     cout<<"Before Sorting : ";
-    for(auto &i:arr)
+    for(auto &i:vec)
         cout<<i<<" ";
     cout<<endl;
 
-    insertionSort(arr);
+    recusive_insertion_sort(vec,0);
 
-    cout<<"After Sorting  : ";
-    for(auto &i:arr)
+    cout<<"After Sorting : ";
+    for(auto &i:vec)
         cout<<i<<" ";
     cout<<endl;
+
     return 0;
 }
-//Problem Link : https://www.geeksforgeeks.org/problems/insertion-sort/1
